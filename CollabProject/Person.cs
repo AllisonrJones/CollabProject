2ae69c17cf8d679bba51503566058ad64942b8a6
jgarr27 <117048949+jgarr27@users.noreply.github.com>
2023-01-25 16:20:10 -0500

    class added

---
+++ CollabProject/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollabProject
+{
+    public class Person
+    {
+        //properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //constructor
+        public Person(string _name, int _Age)
+        {
+            Name = _name;
+            Age = _Age;
+        }
+    }
+}
